f2d351373265de78ef0fb2d0b328c2ddca4e2a3b
Alexey Chernyshev <chernyshev@rg.ru>
2017-08-24 15:14:26 +0300

    movie flow to config and return webpack.config to configs

--- src/configs/common/scripts.config.js
+++ src/configs/common/scripts.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'script',
+    name: 'scripts',
     subTasks: [
 
         {
--- src/configs/common/tmpls.config.js
+++ src/configs/common/tmpls.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'tmpl',
+    name: 'tmpls',
     subTasks: [
 
         {
